195c3b1fbee709ca57c50e90f8d2d69d1925a01a
Jadson Santos <jadsonjs@gmail.com>
2019-06-11 22:09:22 -0300

    stacking not working

--- MachineLearning/ensemble-stacking.py
+++ MachineLearning/ensemble-stacking.py
@@ -18,14 +18,14 @@
 import pandas as pd
 import numpy as np
 
-from sklearn.model_selection import KFold, cross_validate
+from sklearn.model_selection import KFold, cross_validate, cross_val_score
 
 from sklearn.neighbors import KNeighborsClassifier  # KNN
 from sklearn.naive_bayes import GaussianNB          # Naive bayes
 from sklearn.neural_network import MLPClassifier    # MLP
 from sklearn import tree                            # Decision Tree
+from sklearn.linear_model import LogisticRegression # 
 from mlxtend.classifier import StackingClassifier   # Ensemble Stakking
-from sklearn import model_selection
 from mlxtend.feature_selection import ColumnSelector
 
 
@@ -36,23 +36,33 @@ from mlxtend.feature_selection import ColumnSelector
 # read the CSV file with your data base and put into a Pandas DataFrame 
 # https://www.shanelynn.ie/using-pandas-dataframe-creating-editing-viewing-data-in-python/
 #
-df = pd.read_csv('/home/jadson/Documentos/doutorado/machile-learning/WDBC.csv')
+df = pd.read_csv('/Users/jadson/tmp/WDBC_preprocessed.csv')
 #print(df)
+
+#
+# Get the classes of the problem
+#
+classes = df['diagnosis']
+#print(classes)
+
 
 #
 # Define the classifiers 
 #
-classifierKNN = KNeighborsClassifier(n_neighbors=3)
+classifierKNN = KNeighborsClassifier(n_neighbors=1)
 classifierNB = GaussianNB()
 #classifierDT = tree.DecisionTreeClassifier(criterion=criterion, splitter=splitter, min_samples_split=random.random(), max_features=max_features)
 classifierMLP = MLPClassifier(hidden_layer_sizes=(17), learning_rate_init=0.01, max_iter=50, activation='tanh', solver='sgd', momentum=0.8)
+lr = LogisticRegression()
 
 
-sclf = StackingClassifier(classifiers=[classifierKNN, classifierNB, classifierMLP], meta_classifier=classifierKNN)
+#
+# Define the ensemble classifier with its base classifiers
+#
+sclf = StackingClassifier(classifiers=[classifierMLP, classifierMLP, classifierMLP], meta_classifier=classifierKNN)
 
-
-# run the 
-# 10-fold cross validation:
-
-scores = model_selection.cross_val_score(sclf, df, y, cv=10, scoring='accuracy')
+#
+# run the ensemble classification in a 10-fold cross validation:
+#
+scores = cross_val_score(sclf, df, classes, cv=KFold(n_splits=10), scoring='accuracy')
 print("Accuracy: %0.2f (+/- %0.2f) [%s]" % (scores.mean(), scores.std(), 'StackingClassifier'))
